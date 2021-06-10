00701351b0c6535e023c82f74aaa46e42c320c03
shimu88 <85053776+shimu88@users.noreply.github.com>
2021-06-10 20:21:12 +0600

    Add files via upload

---
+++ labtask1/Registration.php
@@ -0,0 +1,54 @@
+<!DocType HTML>
+<html>
+<head>
+<title> Registration Form</title>
+</head>
+<body>
+<form>
+<table>
+<tr>
+<td>FullName:</td>
+<td><input type="text" Name="name"></td>
+</tr>
+<tr>
+<td> Email:</td>
+<td><input type="text" Email="Email"></td>
+</tr>
+<tr>
+<td> password:</td>
+<td><input type="text" password="password"></td>
+</tr>
+<tr>
+<td> comment:</td>
+<td><textarea cols="45" rows="5" name="message"></textarea></td>
+</tr>
+<tr>
+   <td>Gender</td>
+   <td><input type="radio" name="gender">Female
+   <input type="radio" name="gender">Male
+   <input type="radio" name="gender">other
+   </td>
+</tr>
+<tr>
+   <td>Hobby</td>
+   <td><input type="checkbox" name="singing"> singing 
+   <input type="checkbox" name="dancing"> dancing
+   <input type="checkbox" name="reading"> reading 
+   </td>
+</tr>
+<tr>
+  <td>file</td>
+  <td><input type="file" name="photo"></td>
+</tr>
+<tr>
+<td><input type="submit" value="Submit"></td>
+<td><input type="submit" value="Reset"></td>
+</tr>
+
+</table>
+</form>
+</body>
+</html>
+
+
+
