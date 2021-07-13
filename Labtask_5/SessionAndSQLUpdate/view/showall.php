8247dd73dc5c0cd1807482fb4fac4c295a191007
Samima Sultana <85053776+shimu88@users.noreply.github.com>
2021-07-13 20:00:50 +0600

    Add files via upload

---
+++ Labtask_5/SessionAndSQLUpdate/view/showall.php
@@ -0,0 +1,30 @@
+<?php
+session_start(); 
+if(empty($_SESSION["username"])) 
+{
+header("Location: ../control/login.php"); // Redirecting To Home Page
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<body>
+<h2>All Products</h2>
+
+
+<?php 
+include ("../control/showallcheck.php");
+?>
+
+<br/>
+ <h5>Do you want to <a href="../control/logout.php">logout</a></h5>
+
+</body>
+</html>
+
+<?php
+
+
+?>   
